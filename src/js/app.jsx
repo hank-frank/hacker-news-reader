57e4730bcf9e3f32accc6060a67812fd343ba2e5
hankfrank <henryfrank1@yahoo.com>
2019-10-04 12:36:23 -0700

    api calls working, .map in return not

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,60 +2,96 @@ import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 const axios = require('axios');
+import wretch from "wretch"
+//wretch
 
 function App () {
-    const [stories, setStories] = useState(['test', 'test1', 'test2']);
+    const [stories, setStories] = useState(["test", "test2", "test3"]);
 
     useEffect (() => {
-    console.log(`second state `, stories);
-    // console.log(`second state2 `, stories);
+    console.log(`stories from uerEffect: `, stories);
+
     }) 
 
      function call () {
         let storiesArr = [];
          
-        axios.get(`http://hacker-news.firebaseio.com/v0/topstories.json`)
-        .then((result) => {
-            for (let i = 0; i < 30; i++) {
-                axios.get(`http://hacker-news.firebaseio.com/v0/item/${result.data[i]}.json`)
-                .then((result) => {
-                    console.log(result)
-                    storiesArr.push(result);
-                })
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-        })
-        console.log(`storiesArr `, storiesArr);
+        // axios.get(`http://hacker-news.firebaseio.com/v0/topstories.json`)
+        // .then((result) => {
+        //     for (let i = 0; i < 30; i++) {
+        //         axios.get(`http://hacker-news.firebaseio.com/v0/item/${result.data[i]}.json`)
+        //         .then((res) => {
+        //             // console.log(`resposne: `, res)
+        //             storiesArr.push(res);
+        //         })
+        //         setStories(storiesArr)
+        //     }
+        // })
+        // .catch((error) => {
+        //     console.error(error);
+        // })
+
+        // fetch('http://hacker-news.firebaseio.com/v0/topstories.json')
+        //     .then ((res) => res.json())
+        //     .then ((data) => {
+        //         console.log(`data`, data);
+        //         for (let i = 0; i < 30; i++) {
+        //             fetch(`http://hacker-news.firebaseio.com/v0/item/${data[i]}.json`)
+        //             .then((res) => res.json())
+        //             .then((eachStory) => {
+        //                 console.log(eachStory.title)
+        //                 storiesArr.push(eachStory);
+        //             })
+        //         }
+        //     })
+
+        wretch("http://hacker-news.firebaseio.com/v0/topstories.json")
+            .get()
+            .json(data => {
+                // console.log(json)
+                for (let i = 0; i < 20; i++) {
+                wretch(`http://hacker-news.firebaseio.com/v0/item/${data[i]}.json`)
+                    .get()
+                    .json(stories => {
+                        storiesArr.push(stories);
+                    })
+                }
+            })
         setStories(storiesArr);
     }
 
     function test () {
-        console.log(`stories from test button: `, stories)
-        stories.map((each, i) => {
-            return  <tr key={i}>
-                        <td key={i}>{ each.data ? each.data.title : "nope" }</td>
-                    </tr>
+        console.log(stories);
+       
+       stories.map((each) => {
+            console.log(each.title ? each.title : "whoopsie daisy's")
+            return <p>{each.title}</p>
         })
     }
 
         return (
+            
            <>
-                <h1 onClick={ () => call() } className="click">Test</h1>
-                <h1 onClick={ () => test() } className="click">Test2</h1>
-                <table>
+                <h1 onClick={ () => call() } className="click">Fire off API calls</h1>
+                <h1 onClick={ () => test() } className="click">Test state of stories/<br/>.map each title</h1>
+                <table className="table">
                     <thead>
                         <tr>
                             <td>Table</td>
                         </tr>
                     </thead>
                     <tbody>
-                            <tr>
-                                <td>{stories}</td>
-                            </tr>
+                           {
+                                stories.map((each, i) => {
+                                    // console.log(each.title ? each.title : "whoopsie daisy's")
+                                    return <tr key={i}>
+                                            <td>{each.title ? each.title : each}</td>
+                                        </tr>
+                            })
+                           }
                     </tbody>
                 </table>
+               
            </>
         );
 
@@ -63,6 +99,7 @@ function App () {
 
 export default App;
 
+//the different api calls:
 // Items
 // /v0/item/<integerid>.json
 // Users
