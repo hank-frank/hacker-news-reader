575c6fdeb3e2d21e71622475186dadc0596ddc21
hankfrank <henryfrank1@yahoo.com>
2019-10-08 13:56:10 -0700

    moving initial async calls into useEffect in anticipation of pagination

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,81 +4,17 @@ import { Helmet } from 'react-helmet';
 
 import Header from "./header.jsx"
 
-let sampleData = [
-    { 
-        by: "lp001",
-         descendants: 113,
-         id: 21167871,
-         kids: (14) [21169229, 21168671, 21168411, 21168594, 21168278, 21168159, 21168251, 21168732, 21168330, 21168697, 21168210, 21169171, 21168289, 21168629],
-         score: 193,
-         time: 1570308639,
-         title: "Google, Xiaomi, and Huawei affected by zero-day flaw that unlocks root access",
-         type: "story",
-         url: "https://thenextweb.com/security/2019/10/04/google-xiaomi-and-huawei-devices-affected-by-zero-day-flaw-that-unlocks-root-access/"
-     },
-     {
-         by: "danicgross",
-         descendants: 6,
-         id: 21158487,
-         kids: (6) [21169239, 21169225, 21169227, 21169215, 21169078, 21169035],
-         score: 40,
-         time: 1570202112,
-         title: "Streamlit: Turn a Python script into an interactive data analysis tool",
-         type: "story",
-         url: "https://towardsdatascience.com/coding-ml-tools-like-you-code-ml-models-ddba3357eace?source=friends_link&sk=f7774c54571148b33cde3ba6c6310086"
-     },
-     {
-         by: "carrozo",
-         descendants: 33,
-         id: 21167683,
-         kids: (13) [21168130, 21169118, 21168568, 21167918, 21168204, 21168499, 21168585, 21169155, 21168023, 21168273, 21168031, 21168032, 21168861],
-         score: 97,
-         time: 1570306694,
-         title: "The Wave that changed the world (2017)",
-         type: "story",
-         url: "https://www.paloaltoonline.com/news/2017/03/17/the-wave-that-changed-history",
-     }
- ]
-
-
-
-// gets list of Top article ids
-const getTopStoryList = async () => {
-    const res = await fetch(
-      "https://hacker-news.firebaseio.com/v0/topstories.json"
-    );
-    return await res.json();
-  };
-
-//get list of all New article id's
-const getNewStoryList = async () => {
-    const res = await fetch(
-      "https://hacker-news.firebaseio.com/v0/newstories.json"
-    );
-    return await res.json();
-  };
-
-//get list of all New article id's
-const getBestStoryList = async () => {
-    const res = await fetch(
-      "https://hacker-news.firebaseio.com/v0/beststories.json"
-    );
-    return await res.json();
-  };
-
-  // iterates over article list and returns a promise.all
-  const getStories = (articles, quantity) => {
+  const getStories = (articles, start, finish) => {
     return Promise.all(
-      articles.slice(0, quantity).map(async article => {
+      articles.slice(start, finish).map(async article => {
         const storyRes = await fetch(
           `https://hacker-news.firebaseio.com/v0/item/${article}.json`
         );
-        // console.log(`storyRes: `, storyRes)
         return await storyRes.json();
       })
     );
   };
-  // maps your response data
+
   const formatStories = stories =>
     stories.map(({ by, id, url, score, kids, title = "No Title" }) => ({
       id,
@@ -88,44 +24,78 @@ const getBestStoryList = async () => {
       score,
       kids
     }));
+
   function App() {
     const [stories, setStories] = useState([]);
+    const [start, setStart] = useState(0);
+    const [finish, setFinish] = useState(25);
+    const [top, setTop] = useState([]);
+    const [best, setBest] = useState([]);
+    const [recent, setRecent] = useState([]);
+
+    useEffect ( () => {
+      async function fetchTop () {
+      const res = await fetch(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+      );
+        let topData = await res.json();
+        setTop(topData);
+    };
+
+    async function fetchBest () {
+      const res = await fetch(
+        "https://hacker-news.firebaseio.com/v0/beststories.json"
+      );
+        let bestData = await res.json();
+        setBest(bestData);
+    };
+
+    async function fetchNew () {
+      const res = await fetch(
+        "https://hacker-news.firebaseio.com/v0/newstories.json"
+      );
+        let newData = await res.json();
+        setRecent(newData);
+    };
+
+    fetchBest();
+    fetchNew();
+    fetchTop();
+  }, []); 
 
     const callTop = async () => {
-      // first get list of stories
-      const res = await getTopStoryList();
-      // then async request all of the individual articles
-      // and push them into a group of promises
-      const storiesArr = await getStories(res, 25);
-      console.log(`storiesArr Top: `, storiesArr)
-      // then set your state.
+      const storiesArr = await getStories(top, start, finish);
       setStories(formatStories(storiesArr));
-      console.log(`stories array: `, stories)
     };
 
     const callBest = async () => {
-        // first get list of stories
-        const res = await getBestStoryList();
-        // then async request all of the individual articles
-        // and push them into a group of promises
-        const storiesArr = await getStories(res, 25);
-        console.log(`storiesArr Best: `, storiesArr)
-        // then set your state.
+        const storiesArr = await getStories(best, start, finish);
         setStories(formatStories(storiesArr));
-        // console.log(`stories array: `, stories)
       };
 
       const callNew = async () => {
-        // first get list of stories
-        const res = await getNewStoryList();
-        // then async request all of the individual articles
-        // and push them into a group of promises
-        const storiesArr = await getStories(res, 25);
-        console.log(`storiesArr New: `, storiesArr)
-        // then set your state.
+        const storiesArr = await getStories(recent, start, finish);
         setStories(formatStories(storiesArr));
-        console.log(`stories array: `, stories)
       };
+
+      const forward = (start, finish) => {
+            setStart(start + 25);
+            setFinish(finish + 25);
+            callTop();
+      }
+
+      const back = (start, finish) => {
+        if (start > 25) {
+          setStart(start - 25);
+          setFinish(finish - 25);
+        }
+      }
+
+      const test = () => {
+        console.log(`top from test: `, top);
+        console.log(`best: `, best);
+        console.log(`new: `, recent)
+      }
 
     return (
       <div className="App">
@@ -134,10 +104,12 @@ const getBestStoryList = async () => {
             best = {callBest}
             top = {callTop}
           />
+          <button className="test-button" onClick={() => test()}>Test</button>
+          <button className="test-button" onClick={() => back()}>Back</button>
+          <button className="test-button" onClick={() => forward()}>Forward</button>
         <table className="table">
           <tbody>
             {stories.map(story => {
-                // console.log(`each story: `, story)
               return (
                 <tr key={story.id}>
                   <td>
@@ -152,6 +124,8 @@ const getBestStoryList = async () => {
             })}
           </tbody>
         </table>
+        {/* <button className="test-button" onClick={forward()}>Back</button>
+        <button className="test-button" onClick={back()}>Forward</button> */}
       </div>
     );
   }
@@ -163,7 +137,3 @@ export default App;
 // /v0/item/<integerid>.json
 // Users
 // /v0/user/<userid>.json/
-// Top Stories
-// /v0/topstories.json
-
-
